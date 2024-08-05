ecba932232c8ba110f128a064ff79b877f9964f4
micharau <m.rau@hs-osnabrueck.de>
2024-08-05 17:01:20 +0200

    New Assets

--- Assets/Scripts/Doorbell.cs
+++ Assets/Scripts/Doorbell.cs
@@ -1,22 +1,47 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.XR.Interaction.Toolkit;
 using SojaExiles;
 
 public class Doorbell : MonoBehaviour
 {
     public AudioSource doorbellSound;
-    public opencloseDoor doorScript;
+    // public opencloseDoor doorScript;
+    public SojaExiles.opencloseDoor doorScript;
+    public float interactionCooldown = 5.0f; // Time in seconds after which the doorbell can be interacted with again
+    private bool canInteract = true;
 
-    // This method will be called when the doorbell is interacted with
+    //Co-Routine for the door opening with delay
+    IEnumerator ToggleDoorWithDelay()
+    {
+        yield return new WaitForSeconds(5);
+        doorScript.ToggleDoor();
+    }
+    //Co-Routine for interaction cooldown
+    IEnumerator Cooldown()
+    {
+        canInteract = false;
+        yield return new WaitForSeconds(interactionCooldown);
+        canInteract = true;
+    }
+
+    //This method will be called when the doorbell is interacted with
     public void OnPoke()
     {
+        if (!canInteract)
+            return;
+
         if (doorbellSound != null)
         {
             doorbellSound.Play();
         }
         if (doorScript != null)
-        {
-            doorScript.ToggleDoor();
+        {   
+            //open the door with delay
+            //doorScript.ToggleDoor();
+            StartCoroutine(ToggleDoorWithDelay());
         }
+        //Start interaction cooldown
+        StartCoroutine(Cooldown());
     }
 }
